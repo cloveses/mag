87359f6aa969d5dcaf543fe6558efde0891c6ef4
djx <1181564186@qq.com>
2018-02-21 21:46:13 +0800

    e

---
+++ db/common.py
@@ -0,0 +1,9 @@
+import pony.orm
+
+db = pony.orm.Database()
+
+
+
+
+# db.bind(provider='mysql',db='test')
+# db.generate_mapping(create_tables=True)
--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.secret_key = 'lkdjkjf&*^%&(*(*))(775$%^^**(*_+&**&GBHJB'
 
 @app.route('/')
 def hello_world():
